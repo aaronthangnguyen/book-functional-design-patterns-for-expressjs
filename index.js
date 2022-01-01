6e1cc5519008ac6ff46a4164ab5e58ed9ca4a8e9
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-01 01:25:00 -0500

    Apply Router design pattern

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ const emails = require("./fixtures/emails");
 let app = express();
 // express() is a factory for building request handlers.
 
+const getUsersRoute = (req, res) => {
+  res.send(users);
+  // res.send() automically converts list of emails to JSON-formatted strings
+};
+
+const getEmailsRoute = (req, res) => {
+  res.send(emails);
+};
+
+const noRouteFound = (req, res) => {
+  const route = req.method + " " + req.url;
+  res.end(`You asked for ${route}`);
+};
+
+const routes = {
+  "GET /users": getUsersRoute,
+  "GET /emails": getEmailsRoute,
+};
+
 app.use((req, res) => {
   // req: read-only, res: write-only.
-  let route = req.method + " " + req.url;
+  const route = req.method + " " + req.url;
+  const handler = routes[route] || noRouteFound;
 
-  if (route === "GET /users") {
-    console.log(req.accepts());
-    res.send(users);
-    // res.send() automically converts list of emails to JSON-formatted strings
-  } else if (route === "GET /emails") {
-    res.send(emails);
-  } else {
-    res.end("You asked for " + route);
-  }
+  handler(req, res);
 });
 
 app.listen(3000);
