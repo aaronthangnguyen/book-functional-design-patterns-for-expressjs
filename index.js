511b3fbf9346e2e47f7272a389420ff971c96c39
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-01 01:00:40 -0500

    Create first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const users = require("./fixtures/users");
+const emails = require("./fixtures/emails");
+
+let server = http.createServer((req, res) => {
+  // req: read-only, res: write-only
+  let route = req.method + " " + req.url;
+
+  if (route === "GET /users") {
+    res.end(JSON.stringify(users));
+  } else if (route === "GET /emails") {
+    res.end(JSON.stringify(emails));
+  } else {
+    res.end("You asked for " + route);
+  }
+});
+
+server.listen(3000);
