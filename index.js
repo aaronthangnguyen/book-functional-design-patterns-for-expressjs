e5b84687068085e758a111e8c34e04e304ef6e03
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-01 01:41:20 -0500

    Add routes with dynamic segments (params)

--- index.js
+++ index.js
@@ -11,13 +11,27 @@ const getUsersRoute = (req, res) => {
   // res.send() automically converts list of emails to JSON-formatted strings
 };
 
+const getUserRoute = (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+
+  res.send(user);
+};
+
 const getEmailsRoute = (req, res) => {
   res.send(emails);
 };
 
+const getEmailRoute = (req, res) => {
+  const email = emails.find((email) => email.id === req.params.id);
+
+  res.send(email);
+};
+
 const router = express.Router();
 router.get("/users", getUsersRoute);
+router.get("/users/:id", getUserRoute);
 router.get("/emails", getEmailsRoute);
+router.get("/emails/:id", getEmailRoute);
 
 app.use(router);
 app.listen(3000);
