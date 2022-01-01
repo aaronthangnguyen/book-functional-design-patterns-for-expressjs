ce91c7c5d17e33a94abbe249b86bb95c89c5150e
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-01 01:16:25 -0500

    Replace Http with Express.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const http = require("http");
 const express = require("express");
 
 const users = require("./fixtures/users");
 const emails = require("./fixtures/emails");
 
 let app = express();
+// express() is a factory for building request handlers.
 
 app.use((req, res) => {
-  // req: read-only, res: write-only
+  // req: read-only, res: write-only.
   let route = req.method + " " + req.url;
 
   if (route === "GET /users") {
-    res.end(JSON.stringify(users));
+    console.log(req.accepts());
+    res.send(users);
+    // res.send() automically converts list of emails to JSON-formatted strings
   } else if (route === "GET /emails") {
-    res.end(JSON.stringify(emails));
+    res.send(emails);
   } else {
     res.end("You asked for " + route);
   }
 });
 
-let server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
