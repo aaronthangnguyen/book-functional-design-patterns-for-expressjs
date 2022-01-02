e59f0237dcdf5605c76c7e05012778532aa5dc9d
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-02 14:59:18 -0500

    Extract routers to separate files

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
 const express = require("express");
 
-const users = require("./fixtures/users");
-const emails = require("./fixtures/emails");
+// Routers
+const usersRouter = require("./routes/users");
+const emailsRouter = require("./routes/emails");
 
 let app = express();
 // express() is a factory for building request handlers.
 
-const getUsersRoute = (req, res) => {
-  res.send(users);
-  // res.send() automically converts list of emails to JSON-formatted strings
-};
-
-const getUserRoute = (req, res) => {
-  const user = users.find((user) => user.id === req.params.id);
-
-  res.send(user);
-};
-
-const getEmailsRoute = (req, res) => {
-  res.send(emails);
-};
-
-const getEmailRoute = (req, res) => {
-  const email = emails.find((email) => email.id === req.params.id);
-
-  res.send(email);
-};
-
-const usersRouter = express.Router();
-usersRouter.get("/", getUsersRoute);
-usersRouter.get("/:id", getUserRoute);
-
-const emailsRouter = express.Router();
-emailsRouter.get("/", getEmailsRoute);
-emailsRouter.get("/:id", getEmailRoute);
-
 app.use("/users", usersRouter);
 app.use("/emails", emailsRouter);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running at Port 3000.");
+});
