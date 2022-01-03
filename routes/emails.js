579e688cab80e690a6c3f23792953e295ec07f8d
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-03 13:40:33 -0500

    Add create email route

--- routes/emails.js
+++ routes/emails.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const readBody = require("../lib/read-body");
 const emails = require("../fixtures/emails");
+const generateId = require("../lib/generate-id");
 
 const getEmailsRoute = (req, res) => {
   res.send(emails);
@@ -10,9 +12,17 @@ const getEmailRoute = (req, res) => {
   res.send(email);
 };
 
+const createEmailRoute = async (req, res) => {
+  body = await readBody(req);
+  const newEmail = { id: generateId(), ...JSON.parse(body) };
+  emails.push(newEmail);
+  res.status(201).send();
+};
+
 const emailsRouter = express.Router();
 
 emailsRouter.get("/", getEmailsRoute);
 emailsRouter.get("/:id", getEmailRoute);
+emailsRouter.post("/", createEmailRoute);
 
 module.exports = emailsRouter;
